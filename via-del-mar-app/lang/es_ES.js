33370d102afcc5b1cedc61b6fcf50a13deef1d5d
OsmaroB <desarrollo@inventa.com.sv>
2022-10-16 18:14:08 -0600

    add google auth

--- via-del-mar-app/lang/es_ES.js
+++ via-del-mar-app/lang/es_ES.js
@@ -8,6 +8,7 @@ spanish.Pl_In_PassLogin = 'Ingresa la contraseña';
 
 // buttons
 spanish.Btn_Login = 'Iniciar sesión';
+spanish.Btn_Login_Google = 'Iniciar sesión con Google';
 spanish.Btn_ForgotPass = '¿Olvidaste tu contraseña?';
 
 // messages
